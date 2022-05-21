1d305e40902f7e1f180a83c81a0b2c53149ca9a0
TorroSoft <torrozapata@gmail.com>
2022-05-21 13:38:08 -0700

    Survey functionality is already finished

--- ui-challenge/src/components/surveyForm/SurveyForm.jsx
+++ ui-challenge/src/components/surveyForm/SurveyForm.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
+import axioClient from "../../config/axiosClient"
+
 
 
 //Images
@@ -5,8 +8,44 @@ import FormImage from "../../img/img-form.svg"
 
 import "./SurveyForm.css"
 const SurveyForm = () => {
+
+    const [survey, setSurvey] = useState(
+        localStorage.getItem("survey") ?
+            JSON.parse(localStorage.getItem("survey")) :
+            { questionOne: "", questionTwo: "" }
+    );
+    const submitSurvey = async () => {
+        try {
+            const response = await axioClient.post("", survey);
+            const { data } = response;
+            return data;
+        } catch (error) {
+
+        }
+    }
+    const hanelOnChange = (e) => {
+        setSurvey({
+            ...survey,
+            [e.target.name]: e.target.value
+        })
+        const surveyClone = { ...survey }
+        surveyClone[e.target.name] = e.target.value
+        localStorage.setItem("survey", JSON.stringify(surveyClone));
+    }
+    const handleOnSubmit = async(e) => {
+        e.preventDefault();
+        const added = await submitSurvey();
+        if (added.hasOwnProperty("msg")) {
+            setSurvey({ questionOne: "", questionTwo: "" })
+            localStorage.removeItem("survey")
+        }
+
+    }
+
     return (
-        <form className="surveyForm">
+        <form className="surveyForm"
+            onSubmit={handleOnSubmit}
+        >
             <fieldset>
                 <div className="surveyForm__img">
                     <img src={FormImage} alt="Image form" />
@@ -16,15 +55,24 @@ const SurveyForm = () => {
                     <h3>How do you feel?</h3>
                     <div className="options">
                         <label className="option">
-                            <input type="radio" name="questionOne" value={"one"} />
+                            <input type="radio" name="questionOne" value={"one"}
+                                onChange={hanelOnChange}
+                                checked={survey?.questionOne == "one" ? true : false}
+                            />
                             <p>Perfect, thank you!</p>
                         </label>
                         <label className="option">
-                            <input type="radio" name="questionOne" value={"two"} />
+                            <input type="radio" name="questionOne" value={"two"}
+                                onChange={hanelOnChange}
+                                checked={survey?.questionOne == "two" ? true : false}
+                            />
                             <p>Meh, I’m alive</p>
                         </label>
                         <label className="option">
-                            <input type="radio" name="questionOne" value={"three"} />
+                            <input type="radio" name="questionOne" value={"three"}
+                                onChange={hanelOnChange}
+                                checked={survey?.questionOne == "three" ? true : false}
+                            />
                             <p>Worst day ever</p>
                         </label>
                     </div>
@@ -33,11 +81,17 @@ const SurveyForm = () => {
                     <h3>Did you sleep well?</h3>
                     <div className="options">
                         <label className="option">
-                            <input type="radio" name="questionTwo" value={"one"} />
-                            <p>No</p>
+                            <input type="radio" name="questionTwo" value={"one"}
+                                onChange={hanelOnChange}
+                                checked={survey?.questionTwo == "one" ? true : false}
+                            />
+                            <p>Yes</p>
                         </label>
                         <label className="option">
-                            <input type="radio" name="questionTwo" value={"two"} />
+                            <input type="radio" name="questionTwo" value={"two"}
+                                onChange={hanelOnChange}
+                                checked={survey?.questionTwo == "two" ? true : false}
+                            />
                             <p>No</p>
                         </label>
                     </div>
---
+++ ui-challenge/src/config/axiosClient.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+const axiosClientSurvey = axios.create({
+    baseURL: import.meta.env.VITE_URL_BACK
+})
+
+
+export default axiosClientSurvey
